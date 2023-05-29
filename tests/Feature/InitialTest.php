21428ecc3fbc1f2827b9cd70b5eb83ef54189273
Codeholic <irenic.tahir@gmail.com>
2023-05-29 10:30:00 +0500

    Tests

--- tests/Feature/InitialTest.php
+++ tests/Feature/InitialTest.php
@@ -1,8 +1,30 @@
 <?php
+namespace Tests;
 
-namespace Test;
+use App\Modules\Advertisement_Package\src\Http\Requests\StoreRequest;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
 
-class InitialTest
+class StoreRequestTest extends TestCase
 {
+    use RefreshDatabase, WithFaker;
 
+    public function testValidationRules()
+    {
+        $request = new StoreRequest();
+
+        $this->assertEquals([
+            'title' => ['required', 'string', 'max:255'],
+        ], $request->rules());
+    }
+
+    public function testValidationMessages()
+    {
+        $request = new StoreRequest();
+
+        $this->assertEquals([
+            'title.max' => __('Заголовок не должен превышать :max символов'),
+        ], $request->messages());
+    }
 }
